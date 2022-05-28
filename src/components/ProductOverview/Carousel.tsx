0c31e6bf1f3d7f7d8f51650d14174279381a39ef
DenisTru <denistruong1@gmail.com>
2022-05-28 02:33:20 -0500

    removed dots

--- src/components/ProductOverview/Carousel.tsx
+++ src/components/ProductOverview/Carousel.tsx
@@ -13,15 +13,16 @@ export const Carousel = ({styles}: CarouselProps) => {
     <img className="carousel fade" src={photo.url} alt={styles.name}></img>
   </div>
 })}
-  <BsArrowRightSquare className="previous" onClick={()=> {return showCasedPhoto > 0 ? setShowCasedPhoto(showCasedPhoto - 1): ''}} />
-  <BsArrowRightSquare className="next" onClick={() => {return showCasedPhoto < styles.photos.length - 1 ? setShowCasedPhoto(showCasedPhoto + 1): ''}} />
+<BsArrowRightSquare className="previous" onClick={()=> {return showCasedPhoto > 0 ? setShowCasedPhoto(showCasedPhoto - 1): ''}}/>
+<BsArrowRightSquare className="next" onClick={() => {return showCasedPhoto < styles.photos.length - 1 ? setShowCasedPhoto(showCasedPhoto + 1): ''}} />
+  </div>
+  )
+}
+
+/*  {<img className="carousel showcase" src={styles.photos[showCasedPhoto].url } alt={styles.name}></img>}
   <div className='carousel-dots-container'>
   {styles.photos.map((_photo,index) => {
     return <span className={showCasedPhoto === index ? 'dot-active' : 'dot'} onClick={() => setShowCasedPhoto(index)}></span>
   })}
   </div>
-  </div>
-  )
-}
-
-//  {<img className="carousel showcase" src={styles.photos[showCasedPhoto].url } alt={styles.name}></img>}
+*/
