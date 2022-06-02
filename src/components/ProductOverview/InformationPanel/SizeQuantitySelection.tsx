d2947e026523ce903f9f658bb4baa90a5bd820ac
DenisTru <denistruong1@gmail.com>
2022-06-02 17:48:58 -0500

    added quantity dropdown

--- src/components/ProductOverview/InformationPanel/SizeQuantitySelection.tsx
+++ src/components/ProductOverview/InformationPanel/SizeQuantitySelection.tsx
@@ -4,32 +4,51 @@ import { Select } from '@mantine/core';
 import { sizes } from "@mantine/core/lib/components/ActionIcon/ActionIcon.styles";
 
 export const SizeQuantitySelection = ({skus}: SizeQuantitySelectionProps) => {
-//ONLY STOCKED SKUS
-const stockAvailable = Object.entries(skus).filter((size) => {
-  if(size[1]?.quantity) {
-    return size[1]?.quantity
+const [skuSize, setSkuSize] = useState('');
+const [quantity, setQuantity] = useState('')
+//ONLY STOCKED SKUS available
+const sizesAvailable  = Object.entries(skus).filter((sku) => {
+  if(sku[1]?.quantity) {
+    return sku[1]?.quantity;
   }
-  return 0
-})
-//STOCKED SKU SIZES
-const sizesAvailable = stockAvailable.map((item) => {
-  if(item[1]?.size){
+  return 0;
+  }).map((item) => {
+    if(item[1]?.size){
     return {value: item[1]?.size, label: item[1]?.size }
-  }
-  return ''
-});
+    }
+      return ''
+    });
 
-const [value, setValue] = useState('');
+const selectedSKU = Object.entries(skus).filter((sku) => {
+  return sku[1]?.size === skuSize;
+})[0]
+
+const quantityAvailable = selectedSKU ? new Array(selectedSKU[1]?.quantity).fill(null).map((_, i) => {
+  return {value:(i + 1).toString(), label: (i + 1).toString()}
+}) : ''
+console.log(sizesAvailable)
+console.log(quantityAvailable)
   return (
     <>
-    {sizesAvailable.length > 0 ?
+    {
+    sizesAvailable.length > 0 ?
     <Select clearable
-      value={value}
-      onChange={(e: string)=> setValue(e)}
+      value={skuSize}
+      onChange={(e: string) => setSkuSize(e)}
       placeholder="Choose a size"
       data={sizesAvailable}
     />:
       <OutOfStock sizeOrStock={true} />
+    }
+    {
+    quantityAvailable ?
+    <Select clearable
+      value={quantity}
+      onChange={(e: string) => setQuantity(e)}
+      placeholder="Choose a quantity"
+      data={quantityAvailable}
+    />:
+      <OutOfStock sizeOrStock={false} />
     }
     </>
   )
