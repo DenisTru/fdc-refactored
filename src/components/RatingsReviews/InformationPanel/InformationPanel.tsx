98175b0d1fd118b1e6d5760df2006df82f6d769a
DenisTru <denistruong1@gmail.com>
2022-06-25 17:38:28 -0500

    added fractional bars

--- src/components/RatingsReviews/InformationPanel/InformationPanel.tsx
+++ src/components/RatingsReviews/InformationPanel/InformationPanel.tsx
@@ -11,11 +11,11 @@ export const InformationPanel = () => {
     <span className="star-rating"><StarReviews rating={3.5}/></span>
     <p>100% of reviews recommend this product</p>
     <div className="avg-star-ratings">
-      <li><h5>5 stars</h5> <StarBar starName={'star5'}/></li>
-      <li><h5>4 stars</h5> <StarBar starName={'star4'}/></li>
-      <li><h5>3 stars</h5> <StarBar starName={'star3'}/></li>
-      <li><h5>2 stars</h5> <StarBar starName={'star2'}/></li>
-      <li><h5>1 stars</h5> <StarBar starName={'star1'}/></li>
+      <li><h5>5 stars</h5> <StarBar rating={'50%'} starName={'star5'}/></li>
+      <li><h5>4 stars</h5> <StarBar rating={'20%'} starName={'star4'}/></li>
+      <li><h5>3 stars</h5> <StarBar rating={'15%'} starName={'star3'}/></li>
+      <li><h5>2 stars</h5> <StarBar rating={'10%'} starName={'star2'}/></li>
+      <li><h5>1 stars</h5> <StarBar rating={'5%'} starName={'star1'}/></li>
     </div>
 
 
@@ -27,13 +27,16 @@ export const InformationPanel = () => {
 
 type StarBarProps = {
   starName: 'star1'|'star2'|'star3'|'star4'|'star5'
+  rating: string
 }
 
 
-const StarBar = ({starName}: StarBarProps) => {
+const StarBar = ({starName, rating}: StarBarProps) => {
+  const styles = { "--rating": rating} as React.CSSProperties;
+
   return (
     <span className="star-bar">
-      <span className={starName.toString()}>test</span>
+      <span style={styles} className={starName.toString()}/>
     </span>
   )
 }
