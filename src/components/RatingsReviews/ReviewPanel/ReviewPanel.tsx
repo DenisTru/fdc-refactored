efa128d790a30ba825c0170734a7842bf6d19456
DenisTru <denistruong1@gmail.com>
2022-06-26 20:34:18 -0500

    added useState hook and handler for select

--- src/components/RatingsReviews/ReviewPanel/ReviewPanel.tsx
+++ src/components/RatingsReviews/ReviewPanel/ReviewPanel.tsx
@@ -1,22 +1,29 @@
+import { useState } from "react";
 import { Review } from "./Review";
 import './styles/ReviewPanel.scss'
 import { Select } from '@mantine/core';
 import { FiChevronDown } from 'react-icons/fi'
 
 export const ReviewPanel = () => {
+  const [sort, setSort] = useState('relevance');
+
+  const handleSelect = (e : string)  => {
+    setSort(e)
+  }
   return (
     <ol className="review-list">
       <span className="reviewPanel-description">248 reviews, sorted by&nbsp;
       {<Select
       size="sm"
-      placeholder="default val"
+      value={sort}
+      onChange={handleSelect}
       rightSection={<FiChevronDown size="16px"/>}
-      rightSectionWidth={70}
+      rightSectionWidth={60}
       style={{width: '100px', display: 'inline-block', textDecoration: 'underline'}}
       styles={{ rightSection: { pointerEvents: 'none' } }}
       variant="unstyled"
       maxDropdownHeight={280}
-      data={['helpful', 'newest', 'relevant']}/>}
+      data={['helpful', 'newest', 'relevance']}/>}
       </span>
       <li className="first-bullet"><Review/></li>
       <li><Review/></li>
